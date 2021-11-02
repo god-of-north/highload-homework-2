c257a40d1e5c9501930a30dee8c9e6ae9519ec3d
Maksym Lesyk <mnl@industrialinnovationgroup.com>
2021-11-02 20:08:45 +0200

    Tune testing scripts

--- test_mongo.bat
+++ test_mongo.bat
@@ -1,1 +1,1 @@
-D:\Install\httpd-2.4.51-win64-VS16\Apache24\bin\ab.exe -n 100 -c 10 "http://localhost:1337/mongo"
+D:\Install\httpd-2.4.51-win64-VS16\Apache24\bin\ab.exe -n 1000 -c 30 "http://localhost:1337/mongo"
